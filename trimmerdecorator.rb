637fc0f18947c85e07d74d0234679f2d72848726
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-07-25 20:51:30 +0500

    making required changes

--- trimmerdecorator.rb
+++ trimmerdecorator.rb
@@ -4,7 +4,7 @@ require_relative 'capitalizedecorator'
 
 class TrimmerDecorator < BaseDecorator
   def correct_name
-    return @nameable.correct_name[0, 9] if @nameable.correct_name.length > 10
+    return @nameable.correct_name[0, 10] if @nameable.correct_name.length > 10
 
     @nameable.correct_name
   end
