96236f0d094f82f97ea0a9443874a68ee8f92086
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-07-26 01:16:55 +0500

    School Library: Adding Associations

--- student.rb
+++ student.rb
@@ -1,9 +1,16 @@
 require_relative 'person'
 
 class Student < Person
-  def initialize(classroom, age, name = 'unknown', parent_permmission: true)
+  attr_reader :classroom
+
+  def initialize(classroom1, age, name = 'unknown', parent_permmission: true)
     super(age, name, parent_permmission)
+    @classroom = classroom_(classroom1)
+  end
+
+  def classroom_(classroom)
     @classroom = classroom
+    classroom.student.push(self)
   end
 
   def play_hooky
